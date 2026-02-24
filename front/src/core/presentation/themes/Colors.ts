769732d1a6a76e6d3fe856cf868adcb828fef36d
Esteban <122425146+juanespaez@users.noreply.github.com>
2026-02-24 08:43:45 -0500

    Update Colors.ts

--- front/src/core/presentation/themes/Colors.ts
+++ front/src/core/presentation/themes/Colors.ts
@@ -1,3 +1,9 @@
+/**
+ * colors.ts
+ * Central definition of all colors used in the app.
+ * No color value should be written anywhere else in the codebase.
+ * If the brand color changes, this is the only file that needs to be updated.
+ */
 export const colors = {
     primary: '#e00000',       // red - main brand color
     background: '#ffffff',    // white background
@@ -8,4 +14,5 @@ export const colors = {
     borderButton: '#999999',  // outlined button border
     black: '#000000',
     white: '#ffffff',
+
 };
