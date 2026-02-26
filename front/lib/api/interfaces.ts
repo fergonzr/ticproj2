6e4ffe0c1bdc7ca5dc2ed76885776f00bcd0c953
Fernando González Rivero <fernando.gonzalezr@upb.edu.co>
2026-02-26 08:02:08 -0500

    Document useApi hook and api interfaces properly

--- front/lib/api/interfaces.ts
+++ front/lib/api/interfaces.ts
@@ -1,6 +1,17 @@
 import { Alert, EmergencyCase } from "../models";
 
+/**
+ * A listener for changes on the current EmergencyCase
+ */
 export interface EmergencyUpdateListener {
+  /**
+   *
+   * @param alert The Alert object to report the emergency.
+   * @param onStatusChange A callback to handle changes on the
+   * resultant EmergencyCase.
+   * @returns A promise that is fulfilled once the system processes
+   * the alert and generates a proper corresponding EmergencyCase.
+   */
   reportEmergency(
     alert: Alert,
     onStatusChange: (emergencyCase: EmergencyCase) => void,
--- front/lib/api/useApi.tsx
+++ front/lib/api/useApi.tsx
@@ -2,12 +2,23 @@ import { createContext, useContext } from "react";
 import { EmergencyUpdateListener } from "./interfaces";
 import { MockEmergencyUpdateListener } from "./mock";
 
+/**
+ * Content available through the backend API
+ */
 export type ApiContent = {
   emergencyUpdateListener: EmergencyUpdateListener;
 };
 
+/**
+ * Context that provides the API connections.
+ * Currently all implementations alre mocked.
+ */
 export const ApiContext = createContext<ApiContent>({
   emergencyUpdateListener: new MockEmergencyUpdateListener(),
 });
 
+/**
+ * custom hook that provides and ApiContext context.
+ * @returns ApiContext for interacting with the backend.
+ */
 export const useApi = () => useContext(ApiContext);
