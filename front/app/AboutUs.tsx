db528d63b982386e61e6563a11ba8b80d4618dff
Fernando González Rivero <fernando.gonzalezr@upb.edu.co>
2026-03-02 22:10:10 -0500

    Correct some styles in AboutUs

--- front/app/AboutUs.tsx
+++ front/app/AboutUs.tsx
@@ -6,8 +6,9 @@ import SIEELogo from "@/lib/components/SieeLogo";
 export default function AboutUs(): ReactElement {
   return (
     <ScrollView className="flex flex-col align-middle">
+      <View className="h-4"></View>
       <SIEELogo></SIEELogo>
-      <Text className="text-red-700 text-4xl font-bold text-center">
+      <Text className="text-primary text-4xl mt-10 font-bold text-center">
         {str.aboutUsTitle}
       </Text>
       <View className="rounded-2xl border-2 my-10 mx-20 px-10 py-5 flex flex-col gap-4">
